7d10d8b897b6a3e64eb02a1a916543f1ab92d998
Simon Fredriksson <simfre@omegapoint.se>
2021-03-26 15:42:58 +0100

    Continue Angular Tutorial
    Completed moment 2 of the angular tutorial.

--- simons-angular-project/src/app/app.module.ts
+++ simons-angular-project/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
