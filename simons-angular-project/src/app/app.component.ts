0352d70cfd638c707ee77e89822e840182aabf9e
Simon Fredriksson <simfre@omegapoint.se>
2021-03-26 15:51:20 +0100

    Change title
    Changed the title of the project.

--- simons-angular-project/src/app/app.component.ts
+++ simons-angular-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simons-angular-project';
+  title = 'Simons Angular Tutorial';
 }
