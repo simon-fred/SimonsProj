c1ff755b62a7490cc86e29d9abc66bd78851915b
Simon Fredriksson <simfre@omegapoint.se>
2021-03-26 15:50:29 +0100

    Generate hero-detail
    Generated Hero detail.

--- simons-angular-project/src/app/app.module.ts
+++ simons-angular-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule
